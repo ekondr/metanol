b2081d06bd87fd429b36198225aea79447fd1f35
Eugene Kondratyuk <ekondr@gmail.com>
2013-11-16 09:38:38 +0200

    Minor refactoring

--- lib/metanol/engine_controller.rb
+++ lib/metanol/engine_controller.rb
@@ -7,74 +7,57 @@ require 'active_support/dependencies'
 module Metanol
 
   # Engine's controller which has all methods for storing and processing meta tag's data
-  # todo: to refactor the class!
   module EngineController
     extend ActiveSupport::Concern
 
     module ClassMethods
-
       def metanol_options
         @metanol_options ||= {}
       end
 
       def meta(*args)
-        add_meta_tag(*args) do |name, value|
-          add_main_meta name, value
-        end
+        add_meta_tag(:main, *args)
       end
 
       def og_meta(*args)
-        add_meta_tag(*args) do |name, value|
-          add_og_meta name, value
-        end
+        add_meta_tag(:og, *args)
       end
 
       def wm_meta(*args)
-        add_meta_tag(*args) do |name, value|
-          add_wm_meta name, value
-        end
+        add_meta_tag(:wm, *args)
       end
 
       private
 
-      def add_meta_tag(*args)
+      def add_meta_tag(type, *args)
         if args[0].is_a? Hash
           args[0].each do |name, value|
-            yield name, value
+            add_meta_by_type type, name, value
           end
         elsif args.length == 2
           name = args[0].to_sym
           value = args[1].to_sym
-          yield name, value
+          add_meta_by_type type, name, value
         end
       end
 
-      def add_main_meta(name, value)
-        if metanol_options.key? name
-          metanol_options[name].value = value
-        else
-          metanol_options[name] = Meta::Main.new(name, value)
-        end
-      end
-
-      def add_og_meta(name, value)
-        key = "og:#{name}".to_sym
+      def add_meta_by_type(type, name, value)
+        data = meta_data(name)[type]
+        key = data[:key]
         if metanol_options.key? key
           metanol_options[key].value = value
         else
-          metanol_options[key] = Meta::OpenGraph.new(name, value)
+          metanol_options[key] = data[:type].new(name, value)
         end
       end
 
-      def add_wm_meta(name, value)
-        key = "wm:#{name}".to_sym
-        if metanol_options.key? key
-          metanol_options[key].value = value
-        else
-          metanol_options[key] = Meta::Webmaster.new(name, value)
-        end
+      def meta_data(name)
+        {
+            main: { key: name, type: Meta::Main },
+            og: { key: "og:#{name}", type: Meta::OpenGraph },
+            wm: { key: "wm:#{name}", type: Meta::Webmaster }
+        }
       end
-
     end
 
     def meta(*args)
